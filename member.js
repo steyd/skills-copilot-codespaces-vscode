76206ae338084fbe09819a438d59e0d6e82c93d5
steyd <walp3699@gmail.com>
2024-08-02 07:05:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  return {
+    member: [
+      {
+        id: 1,
+        name: 'John Doe',
+        skills: ['javascript', 'nodejs', 'react'],
+      },
+      {
+        id: 2,
+        name: 'Jane Doe',
+        skills: ['html', 'css', 'javascript'],
+      },
+    ],
+  };
+}
